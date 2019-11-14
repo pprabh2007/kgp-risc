9d57583435eb1a1c774e260ca149148300ef8b8a
pprabh2007 <pprabh2007@hotmail.co.uk>
2019-11-15 05:02:39 +0530

    add xor functionality and test bench

---
+++ src/XOR_TB.v
@@ -0,0 +1,57 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:
+//
+// Create Date:   23:06:10 10/28/2019
+// Design Name:   XOR
+// Module Name:   D:/Xilinx_ISE_DS_Win_14.7_1015_1/ALU/XOR_TB.v
+// Project Name:  ALU
+// Target Device:  
+// Tool versions:  
+// Description: 
+//
+// Verilog Test Fixture created by ISE for module: XOR
+//
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+////////////////////////////////////////////////////////////////////////////////
+
+module XOR_TB;
+
+	// Inputs
+	reg [31:0] A;
+	reg [31:0] B;
+
+	// Outputs
+	wire [31:0] C;
+
+	// Instantiate the Unit Under Test (UUT)
+	XOR uut (
+		.A(A), 
+		.B(B), 
+		.C(C)
+	);
+
+	initial begin
+		// Initialize Inputs
+		A = 0;
+		B = 0;
+
+		// Wait 100 ns for global reset to finish
+        A= 9;
+		  B=33;
+		 #10;
+		 A=10;
+		 B=10;
+		// Add stimulus here
+
+	end
+      
+endmodule
+
